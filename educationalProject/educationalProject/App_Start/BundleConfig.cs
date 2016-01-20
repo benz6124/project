d5a36a35bcaf92e812c16401bf35b7929a4e294d
suratchanan <benchbb01@hotmail.com>
2016-01-20 23:11:24 +0700

    ลบ package ที่ไม่จำเป็นออก - 3

--- educationalProject/educationalProject/App_Start/BundleConfig.cs
+++ educationalProject/educationalProject/App_Start/BundleConfig.cs
@@ -8,10 +8,6 @@ namespace educationalProject
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/respond.js"));
 
