92fb48444338a00032f067e9da3af51774f4776d
suratchanan <benchbb01@hotmail.com>
2016-01-20 18:10:00 +0700

    เอา comment ที่ติ่งไว้ใน API AunBook ออก

--- educationalProject/educationalProject/Controllers/AunBookController.cs
+++ educationalProject/educationalProject/Controllers/AunBookController.cs
@@ -19,17 +19,17 @@ namespace educationalProject.Controllers
             object result = datacontext.SelectFileDownloadLink(string.Format("curri_id = '55' and aca_year = 2560"));
             return Ok(datacontext.file_name);
         }
-        //public IHttpActionResult PostToQueryDownloadLinkByCurriculumAcademic(oCurriculum_academic data)
-        //{
-        //    if (data.curri_id == null) return BadRequest();
-        //    object result = datacontext.SelectFileDownloadLink(string.Format("curri_id = '{0}' and aca_year = {1}",data.curri_id,data.aca_year));
-        //    if (result == null)
-        //        return Ok(datacontext.file_name);
-        //    else if(result.ToString().Contains("notfound"))
-        //        return BadRequest("ไม่พบข้อมูลเล่ม AUN ในหลักสูตร-ปีการศึกษาที่เลือก");
-        //    else
-        //        return InternalServerError(new Exception(result.ToString()));
-        //}
+        public IHttpActionResult PostToQueryDownloadLinkByCurriculumAcademic(oCurriculum_academic data)
+        {
+            if (data.curri_id == null) return BadRequest();
+            object result = datacontext.SelectFileDownloadLink(string.Format("curri_id = '{0}' and aca_year = {1}", data.curri_id, data.aca_year));
+            if (result == null)
+                return Ok(datacontext.file_name);
+            else if (result.ToString().Contains("notfound"))
+                return BadRequest("ไม่พบข้อมูลเล่ม AUN ในหลักสูตร-ปีการศึกษาที่เลือก");
+            else
+                return InternalServerError(new Exception(result.ToString()));
+        }
 
         public async Task<IHttpActionResult> PutForUpload()
         {
