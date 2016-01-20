d7640d1d4f6156582aad4249cd6efa277f81d056
suratchanan <benchbb01@hotmail.com>
2016-01-20 22:22:04 +0700

    เอา package ที่ไม่จำเป็นออก - 1

--- educationalProject/educationalProject/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace educationalProject.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- educationalProject/educationalProject/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace educationalProject.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
