34f75e35d2f46ff5966ed4efb3ca20e29b3f22c2
suratchanan <benchbb01@hotmail.com>
2016-01-21 23:35:03 +0700

    ลบ package ที่ไม่จำเป็นออก - 4

--- educationalProject/educationalProject/Global.asax.cs
+++ educationalProject/educationalProject/Global.asax.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace educationalProject
@@ -16,7 +15,6 @@ namespace educationalProject
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             //Set server culture to th-TH to let server represent date in thai-base format
             System.Globalization.CultureInfo.DefaultThreadCurrentCulture = System.Globalization.CultureInfo.CreateSpecificCulture("th-TH");
