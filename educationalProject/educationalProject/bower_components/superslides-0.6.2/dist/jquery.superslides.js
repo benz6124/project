00712e9f424382e284975bc2f9c41fc561343620
benz6124 <benz6124@hotmail.com>
2016-02-10 15:11:29 +0700

    แก้ๆอีก

--- educationalProject/educationalProject/bower_components/superslides-0.6.2/dist/jquery.superslides.js
+++ educationalProject/educationalProject/bower_components/superslides-0.6.2/dist/jquery.superslides.js
@@ -8,7 +8,7 @@ var Superslides, plugin = 'superslides';
 Superslides = function(el, options) {
   this.options = $.extend({
     play: false,
-    animation_speed: 900,
+    animation_speed: 1500,
     animation_easing: 'swing',
     animation: 'slide',
     inherit_width_from: window,
