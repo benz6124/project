6ac8e1b6eba6e8802827ae3b9f92c8140d2f50a7
suratchanan <benchbb01@hotmail.com>
2016-09-25 15:29:04 +0700

    Optimize code in app.js

--- educationalProject/educationalProject/app.js
+++ educationalProject/educationalProject/app.js
@@ -121,12 +121,7 @@ $scope.not_choose_curri_and_year_yet = true;
               $scope.open_modal_primary_not_send();
          }
           $scope.already_login = true;
-    
          });
-    
-
-  	    
-  	
   }
 
     $rootScope.open_modal_primary_not_send = function () {
@@ -166,12 +161,10 @@ $scope.not_choose_curri_and_year_yet = true;
       $scope.already_login = true;
     $rootScope.current_user = user;
      $cookies.putObject("mymy", user.user_id);
-     
   };
 
 
   $scope.watch_only_admin = function(){
-
     if($rootScope.current_user.user_type == 'ผู้ดูแลระบบ'){
       return true;
     }
@@ -185,14 +178,10 @@ $rootScope.curri_that_be_president_in = function(obj){
   if(!$rootScope.current_user){
       return false;
     }
-
      if(angular.isUndefined($rootScope.current_user.president_in)==true){
-         
         return false;
     }
-
     var index;
-
     for(index =0;index<$rootScope.all_curriculums.length;index++){
       if(!!$rootScope.current_user.president_in[$rootScope.all_curriculums[index].curri_id]){
         obj.push($rootScope.all_curriculums[index]);
@@ -206,56 +195,42 @@ $rootScope.president_in_this_curri = function(curri_id){
       return false;
     }
     if(angular.isUndefined($rootScope.current_user.president_in)==true){
-         
         return false;
     }
      if(angular.isUndefined($rootScope.current_user.president_in[curri_id])==true){
-        
         return false;
     }
-
      return true;
 }
 
 $rootScope.right_from_committee_just_curri = function(curri_id,topic_num,from){
-
     if(!$rootScope.current_user){
       return false;
     }
     if(angular.isUndefined($rootScope.current_user.committee_in)==true){
-         
         return false;
     }
      if(angular.isUndefined($rootScope.current_user.committee_in[curri_id])==true){
-        
         return false;
     }
-
     if($rootScope.current_user.committee_privilege[curri_id][topic_num] >= from){
       return true;
     }
-
     return false;
 }
 
-
 $rootScope.president_in_this_curri_and_year = function(curri_id,aca_year){
-
     if(!$rootScope.current_user){
       return false;
     }
     if(angular.isUndefined($rootScope.current_user.president_in)==true){
-         
         return false;
     }
      if(angular.isUndefined($rootScope.current_user.president_in[curri_id])==true){
-        
         return false;
     }
-
      var index;
-     for(index =0;index<$rootScope.current_user.president_in[curri_id].length;index++ ){
-     
+     for(index =0;index<$rootScope.current_user.president_in[curri_id].length;index++){     
         if($rootScope.current_user.president_in[curri_id][index] == aca_year){
             return true;
         }
@@ -264,19 +239,15 @@ $rootScope.president_in_this_curri_and_year = function(curri_id,aca_year){
 }
 
 $rootScope.right_from_committee = function(curri_id,aca_year,topic_num,from){
-
     if(!$rootScope.current_user){
       return false;
     }
     if(angular.isUndefined($rootScope.current_user.committee_in)==true){
-         
         return false;
     }
      if(angular.isUndefined($rootScope.current_user.committee_in[curri_id])==true){
-        
         return false;
     }
-
      var index;
      for(index =0;index<$rootScope.current_user.committee_in[curri_id].length;index++ ){
      
@@ -286,21 +257,17 @@ $rootScope.right_from_committee = function(curri_id,aca_year,topic_num,from){
             }
             else{
               return false;
-            }
-            
+            }       
         }
      }
      return false;
 }
 
   $scope.scan_only_privilege_curri = function(title_code,obj_privilege_curri){
-
     var index;
     var index2;
-
 for(index=0;index<$rootScope.all_curriculums.length;index++){
     for(index2 = 0;index2<$rootScope.have_privilege_in_these_curri[title_code].length ;index2++){
-      
         if($rootScope.all_curriculums[index].curri_id ==$rootScope.have_privilege_in_these_curri[title_code][index2] ){
             obj_privilege_curri.push($rootScope.all_curriculums[index]);break;
         }
@@ -313,37 +280,30 @@ if( $scope.already_login == false){
   return false;
 }
 
-          var ever_been_committee = !!$rootScope.current_user.committee_in;
+   var ever_been_committee = !!$rootScope.current_user.committee_in;
    var ever_been_president = !!$rootScope.current_user.president_in;
  
   if(title_code=='18' || title_code=='16' || title_code=='21' || title_code=='22'|| title_code=='23'|| title_code=='25'|| title_code=='27' || title_code=='29' || title_code=='28' || title_code=='30'){
     if($rootScope.current_user.user_type == 'ผู้ดูแลระบบ'){
       return true;
     }
-
   }
 
   if(title_code=='18' || title_code=='19' || title_code=='20'|| title_code=='24'|| title_code=='26'){
-
-
     if(angular.isUndefined($rootScope.current_user.president_in)==true){
-      
       return false;
     }
-    else{
-          
+    else{          
         return true;
     }
-
   }
 
-  if(title_code=='17' ){
+  if(title_code=='17'){
     if($rootScope.current_user.user_type == 'ผู้ประเมินจากภายนอก'){
       return true;
     }
     return false;
   }
-
 
       if($rootScope.have_privilege_in_these_curri[title_code]){
          if($rootScope.have_privilege_in_these_curri[title_code].length != 0){
@@ -354,85 +314,33 @@ if( $scope.already_login == false){
       var index;
      $rootScope.have_privilege_in_these_curri[title_code] = [];
 
-//Handle privilege with only 2 option (1 = not permitted,2 = permitted)
-      if(title_code=='2' || title_code=='14' || title_code=='1' || title_code=='4' || title_code=='7' || title_code=='28' || title_code=='30'  || title_code=='32'){
+//Handle privilege
+      if(title_code=='2' || title_code=='14' || title_code=='1' || title_code=='4' || title_code=='7' || title_code=='28' || title_code=='30'  || title_code=='32' ||  /*privilege 2 option*/
+      title_code=='3' || title_code=='5' || title_code=='6'|| title_code=='8'|| title_code=='9'|| title_code=='10' || title_code=='11'|| title_code=='12'|| title_code=='13') /*privilege 3-5 option*/{
         for(index=0;index<$rootScope.current_user.curri_id_in.length;index++){
-          if($rootScope.current_user.privilege[$rootScope.current_user.curri_id_in[index]][title_code] ==2){
-           
-            if($rootScope.have_privilege_in_these_curri[title_code].indexOf($rootScope.current_user.curri_id_in[index])==-1){
-                 $rootScope.have_privilege_in_these_curri[title_code].push($rootScope.current_user.curri_id_in[index]);
-               } 
-            
+          //If normal privilege in that curri is more than 1 => Add curri_id to the $rootScope.have_privilege_in_these_curri[title_code]
+          if($rootScope.current_user.privilege[$rootScope.current_user.curri_id_in[index]][title_code] >1){
+            $rootScope.have_privilege_in_these_curri[title_code].push($rootScope.current_user.curri_id_in[index]);continue;
           }
-
 //If user is committee in any curriculum ,the priviledge to open modal will overridden
-           if (ever_been_committee == true){
+           if(ever_been_committee == true){
             if(!!$rootScope.current_user.committee_in[$rootScope.current_user.curri_id_in[index]]){
-
-             if($rootScope.current_user.committee_privilege[$rootScope.current_user.curri_id_in[index]][title_code] ==2){
-          
-               if($rootScope.have_privilege_in_these_curri[title_code].indexOf($rootScope.current_user.curri_id_in[index])==-1){
-                 $rootScope.have_privilege_in_these_curri[title_code].push($rootScope.current_user.curri_id_in[index]);
-               } 
-            
+             if($rootScope.current_user.committee_privilege[$rootScope.current_user.curri_id_in[index]][title_code] >1){
+                 $rootScope.have_privilege_in_these_curri[title_code].push($rootScope.current_user.curri_id_in[index]);continue;
               }
             }
           }
 //If user is president in any curriculum ,the priviledge to open modal will overridden
-                if (ever_been_president == true){
+          if (ever_been_president == true){
             if(!!$rootScope.current_user.president_in[$rootScope.current_user.curri_id_in[index]]){
-                  if($rootScope.have_privilege_in_these_curri[title_code].indexOf($rootScope.current_user.curri_id_in[index])==-1){
                  $rootScope.have_privilege_in_these_curri[title_code].push($rootScope.current_user.curri_id_in[index]);
-               } 
-            }
-          }
-
-        }
-      }
-
-//Handle privilege with 3-5 options
-      else if(title_code=='3' || title_code=='5' || title_code=='6'|| title_code=='8'|| title_code=='9'|| title_code=='10' || title_code=='11'|| title_code=='12'|| title_code=='13'){
-       
-        for(index=0;index<$rootScope.current_user.curri_id_in.length;index++){
-          if($rootScope.current_user.privilege[$rootScope.current_user.curri_id_in[index]][title_code] >=2){
-           
-             if($rootScope.have_privilege_in_these_curri[title_code].indexOf($rootScope.current_user.curri_id_in[index])==-1){
-                 $rootScope.have_privilege_in_these_curri[title_code].push($rootScope.current_user.curri_id_in[index]);
-               } 
-          
-          }
-       if (ever_been_committee == true){
-            if(!!$rootScope.current_user.committee_in[$rootScope.current_user.curri_id_in[index]]){
-
-             if($rootScope.current_user.committee_privilege[$rootScope.current_user.curri_id_in[index]][title_code] >=2){
-           
-                 if($rootScope.have_privilege_in_these_curri[title_code].indexOf($rootScope.current_user.curri_id_in[index])==-1){
-                 $rootScope.have_privilege_in_these_curri[title_code].push($rootScope.current_user.curri_id_in[index]);
-               } 
-            
-              }
-            }
-          }
-
-               if (ever_been_president == true){
-            if(!!$rootScope.current_user.president_in[$rootScope.current_user.curri_id_in[index]]){
-
-           
-                 if($rootScope.have_privilege_in_these_curri[title_code].indexOf($rootScope.current_user.curri_id_in[index])==-1){
-                 $rootScope.have_privilege_in_these_curri[title_code].push($rootScope.current_user.curri_id_in[index]);
-               } 
-            
-            
             }
           }
         }
       }
-
       if($rootScope.have_privilege_in_these_curri[title_code].length != 0){
-      
         return true;
       }
-
       return false;
     }
 
@@ -445,11 +353,6 @@ if( $scope.already_login == false){
     }
   }
 });
-
-
-
-  
-
 
 // app.factory('AuthResolver', function ($q, $rootScope, $state) {
 //   return {
